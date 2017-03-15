586f8961f55b87706fb77717996375dbdb4e15ac
Lucas <>
2017-03-15 09:59:20 -0300

    Telas

---
+++ src/Controller/CadastroAlunos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import View.Principal;
+
+/**
+ *
+ * @author SCT 05
+ */
+public class CadastroAlunos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal principal = new Principal();
+    }
+    
+    
+    
+}
